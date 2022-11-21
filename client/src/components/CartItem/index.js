dccf4b687229a26f52cd34e47f20d2586cc375ad
rrayachhetri <rrayachhetri@gmail.com>
2022-11-21 14:56:43 -0800

    use of IndexedDB for PWA

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import {  REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 
 const CartItem = ({ item }) => {
@@ -11,6 +12,7 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', {...item});
     };
 
     const onChange = e => {
@@ -21,6 +23,7 @@ const CartItem = ({ item }) => {
                 type:REMOVE_FROM_CART,
                 _id: item._id,
             });
+            idbPromise('cart', 'delete', {...item});
         } else {
             dispatch ({
                 type: UPDATE_CART_QUANTITY,
@@ -28,6 +31,7 @@ const CartItem = ({ item }) => {
                 purchaseQuantity: parseInt(value)
 
             });
+            idbPromise('cart', 'put', {...item, purchaseQuantity:parseInt(value)});
         }
     };
 
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
-
+import { idbPromise } from '../../utils/helpers';
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
@@ -13,13 +13,31 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(()=>{
+    // if there is data to be stored
     if(data) {
+      // lets store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      // but let's also take each product and save it to IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+
+      // and else if to check if `loading` is undefined in `useQuery()` Hook 
+    } else if(!loading) {
+      // since we're offline, get all the data from `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing 
+        dispatch ({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   // const products = data?.products || [];
 
